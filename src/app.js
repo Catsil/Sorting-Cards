6aef03a8ed81302091b0b7e5c70b03e4096fe6ed
Catsil <imgloriasilva@gmail.com>
2021-10-21 07:33:03 +0000

    actualizacion de cartas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,48 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const BODY = document.querySelector("body");
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  createcard();
 };
+
+function createcard() {
+  const CARDS = ["♥", "♠", "♣", "♦"];
+  const COLORS = ["RED", "BLACK"];
+  let card = document.createElement("div");
+  card.classList.add("card");
+  BODY.appendChild(card);
+  let element = document.querySelector(".card");
+  let heart = document.createElement("div");
+
+  let cardtype = Math.floor(Math.random() * CARDS.length);
+  let colorcard = "";
+  if (cardtype == 0 || cardtype == 3) {
+    colorcard = COLORS[0];
+  } else {
+    colorcard = COLORS[1];
+  }
+
+  heart.innerHTML = CARDS[cardtype];
+  heart.style.color = colorcard;
+  element.appendChild(heart);
+
+  let number = document.createElement("div");
+  number.innerHTML = Math.floor(Math.random() * 12 + 1);
+  if (number.innerHTML == 10) {
+    number.innerHTML = "J";
+  } else if (number.innerHTML == 11) {
+    number.innerHTML = "Q";
+  } else if (number.innerHTML == 12) {
+    number.innerHTML = "K";
+  } else {
+  }
+
+  element.appendChild(number);
+  let heart2 = document.createElement("div");
+  heart2.innerHTML = CARDS[cardtype];
+  heart2.style.color = colorcard;
+  heart2.classList.add("heart");
+  element.appendChild(heart2);
+}
